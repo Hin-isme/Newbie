7d1785b22a29a0f4d1f1c2bcca9fff474266aea0
Hin-isme <95458907+Hin-isme@users.noreply.github.com>
2023-11-07 18:12:41 +0700

    Hienisme

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'hienisme.3ihqtdc.mongodb.net',
+    DB_USER: 'Hienisme',
+    DB_PASS: 'Hienisme',
+    DB_DATABASE: 'Shoppingonline',
+    JWT_SECRET: 'Hien-ime',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'hienlam1332003@gmail.com', // gmail service
+    EMAIL_PASS: 'djvllphoafwdpivr'
+  };
+  module.exports = MyConstants;
